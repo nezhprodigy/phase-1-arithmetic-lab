f28ed8e109aea9e52548e6b4eebac758aa6659c6
alex <alex.musyoki@student.moringaschool.com>
2023-10-03 17:21:56 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply 
+let num1 = 2
+let num2 = 31
+multiply = num1 * num2
+let random 
+random = Math.floor(Math.random() * 10) + 1
+let mod
+let num3 = 7
+let num4 = 11
+mod = num4 %=num3
+let max
+max = Math.max(1, 12, 13, 16, 20)
+
+
